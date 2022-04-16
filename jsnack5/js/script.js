b2c845836222c6a6132a438f22874b78f9814913
Guidocode <guidobuono@icloud.com>
2022-04-16 17:36:57 +0200

    Correzione generale esercizi

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -12,16 +12,34 @@ console.log(numDispari);
 
 const output = document.querySelector('#numero');
 
+
+let numeroDisp;
+
+let num;
+
 for(i = 0; i < promptLimit; i++){
-  const num = parseInt(prompt('Inserisci un numero'));
+  num = parseInt(prompt('Inserisci un numero'));
   console.log(num);
 
+  numeroDisp = false;
+
   if(num % 2 !== 0){
+    numeroDisp = true;
     numDispari.push(num)
-    output.innerHTML = `Tra i numeri che hai inserito questi sono dispari: ${numDispari}`;
-  }else if(num % 2 === 0){
-    output.innerHTML = `Non hai inserito numeri dispari...`;
   }
+
+  console.log(numeroDisp);
+
+  
 }
 
+// IN CONSOLE FUNZIONA
+// L'OUTPUT VIENE STAMPATO NEL MODO CORRETTO SOLO QUANDO L'ULTIMO NUMERO INSERITO E' DISPARI
 
+if(numeroDisp){
+  output.innerHTML = `Tra i numeri che hai inserito questi sono dispari: ${numDispari}`;
+}else{
+  output.innerHTML = `Non hai inserito numeri dispari...`;
+}
+
+console.log(numDispari);
